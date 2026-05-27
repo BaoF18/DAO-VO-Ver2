fafd3ff06c01a158d5f54ecb2bfd9737090ec920
PHAN-THIEN-BAO <phanthienbao0612@gmail.com>
2026-05-27 12:38:33 +0700

    add menu

---
+++ Assets/Scripts/UI/CameraShake.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [Header("Wave Settings")]
+    [Tooltip("Biên độ xoay (roll)")]
+    public float rotationAmplitude = 2f;
+    [Tooltip("Tốc độ xoay")]
+    public float rotationFrequency = 0.5f;
+
+    private Quaternion initialRotation;
+
+    void Start()
+    {
+        initialRotation = transform.localRotation;
+    }
+
+    void Update()
+    {
+        float roll = Mathf.Sin(Time.time * rotationFrequency) * rotationAmplitude;
+        transform.localRotation = initialRotation * Quaternion.Euler(0, 0, roll);
+    }
+}
